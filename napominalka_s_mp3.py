d1252acd283dc26c114200f3c082a09b262733a3
Del1chka <kovalevadelya@mail.ru>
2024-09-07 21:59:16 +0300

    Создаем главное окно

---
+++ napominalka_s_mp3.py
@@ -0,0 +1,17 @@
+#Библиотека для добавления медиа - pygame:
+from tkinter import *
+from tkinter import messagebox as mb
+from tkinter import simpledialog as sd
+import datetime
+import time
+import pygame
+
+window = Tk()
+window.title("Напоминание")
+
+label=Label(text="Установите напоминание.")
+Label.pack(pady=10)
+set_button=Button(text="Установить напоминание", command=set)
+set_button.pack()
+
+
