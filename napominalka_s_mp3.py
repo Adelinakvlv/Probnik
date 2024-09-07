c755c077b1657d9297d60156d1897fb3ad402dc3
Del1chka <kovalevadelya@mail.ru>
2024-09-07 22:14:39 +0300

    Добавлена функция установки времени. Проранено, работает.

--- napominalka_s_mp3.py
+++ napominalka_s_mp3.py
@@ -7,16 +7,27 @@ import time
 import pygame
 
 def set():
-    rem=sd.askstring("Время напоминания", "Введите время напоминания в формате чч:мм (в 24ч. формате")
+    rem=sd.askstring("Время напоминания", "Введите время напоминания в формате чч:мм (в 24ч. формате).")
     #появляется строка с запросом ввода
-
+    if rem:
+        try:
+            hour=int(rem.split(":")[0]) #Делим введенную строку по символу : и часы начинаются до дветочия
+            minute=int(rem.split(":")[1]) #Делим введенную строку по символу : и мин начинаются после дветочия
+            now=datetime.datetime.now()
+            print(now)
+            dt=now.replace(hour=hour, minute=minute)
+            print(dt)
+            t=dt.timestamp()
+            print(t)
+        except Exception as e:
+            mb.showerror("Ошибка!", f"Произошла ошибка {e}")
 
 
 window = Tk()
 window.title("Напоминание")
 
 label=Label(text="Установите напоминание.")
-Label.pack(pady=10)
+label.pack(pady=10)
 set_button=Button(text="Установить напоминание", command=set)
 set_button.pack()
 
