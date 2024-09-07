1b3cd157fd96cf4680286016222540c32960210b
Del1chka <kovalevadelya@mail.ru>
2024-09-07 22:05:16 +0300

    Создаем главное окно

--- napominalka_s_mp3.py
+++ napominalka_s_mp3.py
@@ -6,6 +6,12 @@ import datetime
 import time
 import pygame
 
+def set():
+    rem=sd.askstring("Время напоминания", "Введите время напоминания в формате чч:мм (в 24ч. формате")
+    #появляется строка с запросом ввода
+
+
+
 window = Tk()
 window.title("Напоминание")
 
@@ -13,5 +19,8 @@ label=Label(text="Установите напоминание.")
 Label.pack(pady=10)
 set_button=Button(text="Установить напоминание", command=set)
 set_button.pack()
+
+window.mainloop()
+
 
 
